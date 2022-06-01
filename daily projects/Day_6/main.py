40bf3702df3c0127d895ba7314eaf7ebc3805629
Ashwin Mandaokar <65269769+ashwinam@users.noreply.github.com>
2022-06-01 15:56:24 +0000

    Day 6 reeborgs world challenges completed

---
+++ daily projects/Day_6/main.py
@@ -0,0 +1,3 @@
+"""today I did not build anything but do something interesting and that is reeborg's world(it's a game, where you use programming logic to solve these stages)"""
+
+# link https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
