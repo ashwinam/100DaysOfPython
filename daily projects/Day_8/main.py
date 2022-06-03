fbee7649a8d4139676b0a3ed16800d3ee9eec762
Ashwin Mandaokar <65269769+ashwinam@users.noreply.github.com>
2022-06-03 15:06:52 +0000

    first task cipher text completed

---
+++ daily projects/Day_8/main.py
@@ -0,0 +1,16 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):  
+  cipher_text = ""
+  for each_letter in text:
+    get_index = alphabet.index(each_letter)
+    cipher_text += alphabet[get_index + shift]
+
+  print(cipher_text)
+
+encrypt(text = text, shift = shift)
