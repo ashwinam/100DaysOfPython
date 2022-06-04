e619c0c55799b2211c5e6fe54280366966693c61
Ashwin Mandaokar <65269769+ashwinam@users.noreply.github.com>
2022-06-04 14:30:16 +0000

    update code snippets in day 9

--- daily projects/Day_9/main.py
+++ daily projects/Day_9/main.py
@@ -5,15 +5,19 @@ print(art.logo) #logo
 
 bidders_information = {}
 
-def biggest_bidder():
+def biggest_bidder(bid):
   max_bidder = 0
-  for each_item in bidders_information:
-    if max_bidder < bidders_information[each_item]:
-      max_bidder = bidders_information[each_item]
+  biggest_bidder = ""
+  for each_item in bid:
+    if max_bidder < bid[each_item]:
+      max_bidder = bid[each_item]
+      biggest_bidder = each_item # from angela code
+      
+  print(f"The winner is {biggest_bidder} with a bid of ${max_bidder} ")
 
-  for each_item in bidders_information:
-    if max_bidder == bidders_information[each_item]:
-      print(f"The winner is {each_item} with a bid of ${max_bidder} ")
+  # mine execution
+  # for each_item in bidders_information:
+  #   if max_bidder == bidders_information[each_item]:
   
 # main execution
 should_continue = True
@@ -21,12 +25,13 @@ while should_continue:
   name = input("What is your name?: ")
   bid = int(input("What is your bid? $"))
   
-  bidders_information .update({name:bid})
+  # bidders_information .update({name:bid}) # my execution
+  bidders_information[name] = bid #angela execution
   result = input("Are there any bidders? type 'yes' or 'no'. ")
   
   if result == "no":
     should_continue = False
+    biggest_bidder(bidders_information)
   else:
     clear()
 
-biggest_bidder()
