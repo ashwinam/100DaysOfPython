5b097e96d3d5fc01667d1f115b2e05f8a8defba7
Ashwin Mandaokar <65269769+ashwinam@users.noreply.github.com>
2022-06-05 12:46:38 +0000

    Day 10 project completed

--- daily projects/Day_10/main.py
+++ daily projects/Day_10/main.py
@@ -1,3 +1,9 @@
+import art
+
+# logo
+print(art.logo)
+
+
 # Calculator
 
 # Add
@@ -23,17 +29,48 @@ operations_dict = {
   "/" : divide
 }
 
+def calculation():
+  num1 = float(input("What's the first number? "))
+  
+  # retrive the keys from dictionery
+  for keys in operations_dict:
+    print(keys)
+  
+  should_continue = True
+  
+  while should_continue:
+  
+    operations_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number? "))
+    
+    function = operations_dict[operations_symbol]
+    answer = function(num1, num2)
+    
+    print(f"{num1} {operations_symbol} {num2} = {answer}")
+  
+    if input(f'type "y" to continue with {answer}, or type "n" to start a new calculation:  ') == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculation()
 
-num1 = int(input("What's the first number? "))
+calculation()
+  
 
-# retrive the keys from dictionery
-for keys in operations_dict:
-  print(keys)
 
-operations_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number? "))
 
-function = operations_dict[operations_symbol]
-answer = function(num1, num2)
 
-print(f"{num1} {operations_symbol} {num2} = {answer}")
+
+
+
+# operations_symbol = input("Pick an operation: ")
+  
+#   num3 = int(input("What's the third number? "))
+  
+#   function = operations_dict[operations_symbol]
+  
+#   # second_answer = function(function(num1, num2), num3) # bug
+  
+#   second_answer = function(first_answer, num3)
+  
+#   print(f"{first_answer} {operations_symbol} {num3} = {second_answer}")
