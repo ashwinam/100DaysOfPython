a15975f096fa981699860b72c29020d030cd1a1e
Ashwin Mandaokar <65269769+ashwinam@users.noreply.github.com>
2022-06-07 15:12:08 +0000

    day 12 minor updates

--- daily projects/Day 12/main.py
+++ daily projects/Day 12/main.py
@@ -87,9 +87,9 @@ def game():
   print(logo)
   #Choosing a random number between 1 and 100.
   print("Welcome to the Number Guessing Game!")
-  print("I'm thinking of a number between 1 and 100.")
+  print("The number between 1 and 100.")
   answer = randint(1, 100)
-  print(f"Pssst, the correct answer is {answer}") 
+  # print(f"Pssst, the correct answer is {answer}") 
 
   turns = set_difficulty()
   #Repeat the guessing functionality if they get it wrong.
