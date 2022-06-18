acf88b50f5d3917687f6fe2b7b05ef842eb78675
ashwin <ashwinam31@gmail.com>
2022-06-18 18:48:02 +0530

    turtle crossing game project setup

---
+++ daily projects/Day 23/turtle crossing game/main.py
@@ -0,0 +1,24 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Cross The Road")
+screen.tracer(0)
+
+player = Player()
+car = CarManager()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+
+is_game_on = True
+while is_game_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+screen.exitonclick()
