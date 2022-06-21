2ca1d9581099ce0555172f8af6a022e8bcf52893
ashwin <ashwinam31@gmail.com>
2022-06-21 07:57:23 +0530

    screen setup

--- extra projects/Guess Indian States/main.py
+++ extra projects/Guess Indian States/main.py
@@ -1,4 +1,13 @@
 import turtle
 
+screen = turtle.Screen()
+screen.setup(width=800, height=800)
+image = "india_blank_map_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+screen.exitonclick()
 
 
