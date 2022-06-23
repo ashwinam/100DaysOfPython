52d494ba90ecd961a3b7dbdf725f4edebc8c4854
ashwin <ashwinam31@gmail.com>
2022-06-23 21:03:51 +0530

    pomodoro ui is completed

---
+++ daily projects/Day 28/Pomodoro/main.py
@@ -0,0 +1,57 @@
+import tkinter
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Timer Label
+timer_label = tkinter.Label()
+timer_label.config(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=1, row=0)
+
+
+
+
+# canvas
+canvas = tkinter.Canvas()
+canvas.config(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)  # Image need to set in x & Y coordinate
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)  # establish image on window
+
+
+# buttons
+start_btn = tkinter.Button()
+start_btn.config(text="start")
+start_btn.grid(column=0, row=2)
+
+reset_btn = tkinter.Button()
+reset_btn.config(text="reset")
+reset_btn.grid(column=2, row=2)
+
+# check mark label
+check_mark_label = tkinter.Label()
+check_mark_label.config(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+check_mark_label.grid(column=1, row=3)
+
+window.mainloop()
+
+
