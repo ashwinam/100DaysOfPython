d770687d5e22f037475739ace86edb99e37b0bc9
ashwin <ashwinam31@gmail.com>
2022-06-24 20:27:23 +0530

    logo setup

---
+++ daily projects/Day 29/Password Generator/main.py
@@ -0,0 +1,21 @@
+import tkinter
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tkinter.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+# canvas
+canvas = tkinter.Canvas()
+canvas.config(width=200, height=200)
+logo = tkinter.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.pack()
+
+
+window.mainloop()
